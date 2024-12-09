058f6f6af2a2cff152074b961b0556fc5b3ed4bc
pavelxw <nahid.pavel@xen.works>
2024-12-09 11:41:53 +0600

    styles changes for  home and tickets page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p>Tickets App</p>
-      <Link href={"/tickets"} className="underline">
-        Go To Tickets
-      </Link>
+    <div className="flex flex-col flex-1 gap-y-8">
+      <div>
+        <p className="text-3xl font-bold tracking-tighter">HomePage</p>
+        <p className="text-sm text-muted-foreground">This is for home page</p>
+      </div>
+
+      <div className="flex flex-1 flex-col items-center">
+        <Link href={"/tickets"} className="underline">
+          Go To Tickets
+        </Link>
+      </div>
     </div>
   );
 }
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,17 +3,32 @@ import { initialTickets } from "@/app/data";
 import Link from "next/link";
 
 const Tickets = () => {
-  return initialTickets.map((item) => {
-    return (
-      <div key={item?.id}>
-        <p>{item?.title}</p>
-
-        <Link href={`/tickets/${item?.id}`} className="underline">
-          View
-        </Link>
+  return (
+    <div className="flex flex-1 flex-col gap-y-8">
+      <div>
+        <p className="text-3xl font-bold tracking-tighter">Tickets Page</p>
+        <p className="text-sm text-muted-foreground">This is for ticket page</p>
       </div>
-    );
-  });
+      <div className="flex flex-1 flex-col items-center gap-y-5">
+        {initialTickets.map((item) => {
+          return (
+            <div
+              key={item?.id}
+              className="max-w-[450px] p-6 border border-slate-400 border-rounded w-full"
+            >
+              <h3 className="text-lg font-semibold">{item?.title}</h3>
+              <p className="truncate text-slate-700">{item?.content}</p>
+
+              <Link href={`/tickets/${item?.id}`} className="underline">
+                View
+              </Link>
+            </div>
+          );
+        })}
+        ;
+      </div>
+    </div>
+  );
 };
 
 export default Tickets;
